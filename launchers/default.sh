07377c55ba866c42b40643d9d99cbf336fac03bf
peiny51 <ningyuan@ualberta.ca>
2023-03-14 12:13:11 -0600

    solution

--- launchers/default.sh
+++ launchers/default.sh
@@ -13,7 +13,7 @@ dt-launchfile-init
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
 # launching app
-dt-exec roslaunch duckiebot_detection duckiebot_detection_node.launch
+dt-exec roslaunch duckiebot_detection duckiebot_detection_node.launch veh:=csc22932
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
